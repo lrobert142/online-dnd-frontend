7e7db9f1b5fe8c5a7331cb7948014bcc901621fb
Lachlan Robertson <lachlan.robertson142@gmail.com>
2025-07-10 02:11:25 +1000

    Try additional config options to fix images and links

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+    output: 'export',
     basePath: process.env.PAGES_BASE_PATH,
+    allowedDevOrigins: ['192.168.0.112'],
+    images: {
+        unoptimized: true,
+    },
+    trailingSlash: true,
 };
 
 export default nextConfig;

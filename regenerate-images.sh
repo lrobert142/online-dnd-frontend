4c7438bf03161f51593b3e6e168c62ba6afbbcc9
Lachlan Robertson <lachlan.robertson142@gmail.com>
2025-07-10 03:37:01 +1000

    Try using imagemagick directly

--- regenerate-images.sh
+++ regenerate-images.sh
@@ -12,9 +12,8 @@ mkdir npc/200x300
 mkdir pc/400x600
 
 # Create images in the right sizes (do not modify the original files)
-# We use `mogrify` directly instead of `magick mogrify` to ensure this is compatible with GitHub actions
-mogrify -path npc/200x300 -resize 200x300 npc/*
-mogrify -path pc/400x600 -resize 400x600 pc/*
+magick mogrify -path npc/200x300 -resize 200x300 npc/*
+magick mogrify -path pc/400x600 -resize 400x600 pc/*
 
 # Done!
 echo "Regenerated all images!"
